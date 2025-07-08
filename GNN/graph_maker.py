Fixed masking, model testing

GNN/graph_maker.py
@@ -22,7 +22,7 @@ max_len = np.max(lens)
 
 ### Set data directory, batch size, and maximum number of nodes (galaxies)
 DATA_DIR   = "/projects/mccleary_group/habjan.e/TNG/Data/GNN_SBI_data/graph_data/"
-BATCH_SIZE = 8                 
+BATCH_SIZE = 64                 
 MAX_NODES  = 500
 LATENT_SIZE = 128
 
@@ -76,13 +76,19 @@ def pad_single(g, max_nodes):
                                  n_node=max_nodes,
                                  n_edge=0)
 
+def explicit_mask(g, max_nodes):
+    n = g.nodes.shape[0]
+    return jnp.concatenate([jnp.ones(n, bool),
+                            jnp.zeros(max_nodes - n, bool)])
+
 
 ### This function pads the graph data so that they have the same input size
 def pad_batch(graphs, max_nodes):
 
     padded_list = [pad_single(g, max_nodes) for g in graphs]
+    mask_list   = [explicit_mask(g, max_nodes) for g in graphs]
 
-    return jraph.batch(padded_list)
+    return jraph.batch(padded_list), jnp.concatenate(mask_list) 
 
 
 ### This function pads the target lists
@@ -112,9 +118,9 @@ def main(fits_file, prefix):
 
         if batch_counter == BATCH_SIZE:
             # --- pad & save ------------------------------------------
-            padded_graph   = pad_batch(batch_graphs, MAX_NODES)
+            padded_graph, node_mask = pad_batch(batch_graphs, MAX_NODES)
             padded_targets = pad_targets(batch_targets,BATCH_SIZE, MAX_NODES)
-            node_mask = jraph.get_node_padding_mask(padded_graph)
+            #node_mask = jraph.get_node_padding_mask(padded_graph)
 
             out_path = DATA_DIR + prefix + f"_batch_{file_counter:04d}.pkl"
 
@@ -135,10 +141,10 @@ def main(fits_file, prefix):
     # If any graphs left that didn’t make a full batch, save them too
     if len(batch_graphs) > 0:
 
-        padded_graph   = pad_batch(batch_graphs, MAX_NODES)
+        padded_graph, node_mask = pad_batch(batch_graphs, MAX_NODES)
         padded_targets = pad_targets(batch_targets, len(batch_graphs), MAX_NODES)
 
-        node_mask = jraph.get_node_padding_mask(padded_graph)
+        #node_mask = jraph.get_node_padding_mask(padded_graph)
 
         out_path = DATA_DIR + prefix + f"_batch_{file_counter:04d}.pkl"
 

GNN/train_gnn.py
@@ -21,7 +21,7 @@ from training_structure import train_model, create_dataloader
 from gnn import GraphConvNet
 
 ### Add a suffix for a new model
-suffix = '_40'
+suffix = '_100'
 
 ### Import data and create data loaders
 batch_file_path = "/projects/mccleary_group/habjan.e/TNG/Data/GNN_SBI_data/graph_data"
@@ -29,8 +29,8 @@ batch_file_path = "/projects/mccleary_group/habjan.e/TNG/Data/GNN_SBI_data/graph
 ### Train model
 if __name__ == "__main__":
     # Define hyperparameters
-    epochs = 40
-    learning_rate = 1e-3
+    epochs = 100
+    learning_rate = 1e-2
  
     # Create and train the model
     model = GraphConvNet(latent_size = 128, hidden_size = 256, num_mlp_layers = 3, message_passing_steps = 5)
@@ -49,6 +49,6 @@ if __name__ == "__main__":
     with open(save_path, 'wb') as f:
         pickle.dump(trained_state.params, f)
     
-    save_data = os.getcwd() + '/Loss_arrays/'
+    save_data = '/home/habjan.e/TNG/Sandbox_notebooks/phase_space_recon/Loss_arrays/'
     np.save(save_data + 'train_loss' + suffix + '.npy', train_losses)
     np.save(save_data + 'test_loss' + suffix + '.npy', test_losses)

GNN/training_structure.py
@@ -22,7 +22,7 @@ def create_dataloader(data_dir: str, prefix: str, shuffle: bool = True,) -> Iter
     Each *.pkl file is already one fixed-size batch, so the iterator
     simply loads one file at a time and returns:
         • graph_batch  (jraph.GraphsTuple)
-        • targets      (float32 array,  shape: B*MAX_NODES × 3)
+        • targets      (float32 array,  shape: B*MAX_NODES x 3)
         • node_mask    (float32 array,  length:  B*MAX_NODES)
 
     Parameters
@@ -44,6 +44,8 @@ def create_dataloader(data_dir: str, prefix: str, shuffle: bool = True,) -> Iter
     files   = sorted(glob.glob(pattern))
     if shuffle:
         random.shuffle(files)
+    
+    count = 0
 
     for fname in files:
         with open(fname, "rb") as fh:
@@ -55,6 +57,12 @@ def create_dataloader(data_dir: str, prefix: str, shuffle: bool = True,) -> Iter
             saved["targets"],      # jnp.ndarray
             saved["node_mask"],    # jnp.ndarray
         )
+
+        count += 1
+
+        if count == 10:
+            break
+
 
 
 def create_train_state(model, rng_key, learning_rate, example_graph):
@@ -131,13 +139,14 @@ def train_model(
         ### Training
         count = 0
         total_loss = 0
-        for graph, tgt, mask in create_dataloader(data_dir, train_prefix, shuffle=True):
+        for graph, tgt, mask in create_dataloader(data_dir, train_prefix, shuffle=False):
 
             state = train_step(state, graph, tgt, mask, rng_key)
             current_loss = eval_step(state, graph, tgt, mask)
             
             count += 1
             total_loss += float(current_loss)
+
         
         ave_train_loss = float(total_loss / max(count, 1))
         train_losses.append(ave_train_loss)
@@ -168,6 +177,8 @@ def predict(model, params, data_dir, data_prefix = 'test'):
     predictions = []
     mask_arr = []
     tgt_arr = []
+
+    count = 0
         
     # Get predictions batch by batch
     for graph, tgt, mask in create_dataloader(data_dir, data_prefix, shuffle=False):
@@ -178,7 +189,11 @@ def predict(model, params, data_dir, data_prefix = 'test'):
         tgt_arr.append(tgt)
         mask_arr.append(mask)
 
-        break
+        count += 1
+
+        if count == 10:
+            
+            break
         
     # Concatenate all batch predictions
     return jnp.concatenate(predictions, axis=0).squeeze(), jnp.concatenate(tgt_arr, axis=0).squeeze(), jnp.concatenate(mask_arr, axis=0).squeeze()